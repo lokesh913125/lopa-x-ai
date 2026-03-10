5d87e238138aae5f2d4a2f2b4a4d831b3a40429c
lokesh patel <lokeshluckypatel531@gmail.com>
2026-03-10 18:11:47 +0530

    Fix supabase URL in supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://phxpqymsfqpywzffjave.supabase.co";
+const supabaseUrl = "https://phpxqymsfqpywzffjave.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBocHhxeW1zZnFweXd6ZmZqYXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMDIxNDEsImV4cCI6MjA4ODY3ODE0MX0.pRTXkCm8xxTiDDErHb3zt10gHAduK66Jk5PaAXFemzU";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
